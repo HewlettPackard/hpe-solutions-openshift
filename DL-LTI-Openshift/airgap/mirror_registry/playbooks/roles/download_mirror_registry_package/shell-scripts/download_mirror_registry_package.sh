0fd9be48f77f5143551294d2d66b27427b8a05e1
vanandhpe <99195513+vanandhpe@users.noreply.github.com>
2024-10-22 10:24:07 +0530

    Delete DL-LTI-Openshift/airgap directory

--- DL-LTI-Openshift/airgap/mirror_registry/playbooks/roles/download_mirror_registry_package/shell-scripts/download_mirror_registry_package.sh
+++
@@ -1,4 +0,0 @@
-echo "Downloading Mirror Registry"
-cd /usr/local/bin/
-wget https://mirror.openshift.com/pub/cgw/mirror-registry/latest/mirror-registry-amd64.tar.gz
-tar -xvf mirror-registry-amd64.tar.gz
